d27c1e041a1fc9323718e71ceef603de9844279d
loneferret <loneferret@users.noreply.github.com>
2017-04-30 12:04:40 -0400

    Add files via upload

---
+++ classes/bcolours.py
@@ -0,0 +1,26 @@
+# Class starts here
+
+class bcolours:
+	# Got the idea from PTF from Dave Kennedy
+	# https://github.com/trustedsec/ptf
+	# I just wrote colour correctly...
+
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERL = '\033[4m'
+    ENDC = '\033[0m'
+    backBlack = '\033[40m'
+    backRed = '\033[41m'
+    backGreen = '\033[42m'
+    backYellow = '\033[43m'
+    backBlue = '\033[44m'
+    backMagenta = '\033[45m'
+    backCyan = '\033[46m'
+
+    # Class stops here
